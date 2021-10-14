e343f3094cbe582c2d620529fe8707ea42d50419
zhuwei6 <Zhuwei521>
2021-10-14 10:35:27 +0800

    add err

--- internal/app/migrate.go
+++ internal/app/migrate.go
@@ -27,8 +27,7 @@ func init() {
 	if !ok || len(databaseURL) == 0 {
 		log.Fatalf("migrate: environment variable not declared: PG_URL")
 	}
-
-	databaseURL += "?sslmode=disable"
+	// databaseURL += "?sslmode=disable"
 
 	var (
 		attempts = _defaultAttempts
